dd1c6fa21212d8cbbee2a8f1fa7f6284e3297d53
root <root@localhost>
2026-07-29 06:58:11 +0700

    Cloud Build Request - 2026-07-29 06:58:11

--- app/src/main/java/net/openvpn/openvpn/OpenVPNClient.java
+++ app/src/main/java/net/openvpn/openvpn/OpenVPNClient.java
@@ -1024,7 +1024,7 @@ private void renameOvpnFilesToEncoded() {
             ProfileList proflist = profile_list();
             Profile prof = null;
             if (proflist == null || proflist.size() <= 0) {
-                this.profile_group.setVisibility(View.GONE);
+                if (this.profile_group != null) this.profile_group.setVisibility(View.GONE);
             } else {
                 ProfileSource ps = ProfileSource.UNDEF;
                 SpinUtil.show_spinner(this, this.profile_spin, proflist.profile_names());
@@ -1060,9 +1060,9 @@ private void renameOvpnFilesToEncoded() {
                 if (ps != ProfileSource.SPINNER) {
                     SpinUtil.set_spinner_selected_item(this.profile_spin, prof.get_name());
                 }
-                this.profile_group.setVisibility(View.VISIBLE);
-                this.profile_spin.setEnabled(!active);
-                this.profile_edit.setVisibility(active ? View.GONE : View.VISIBLE);
+                if (this.profile_group != null) this.profile_group.setVisibility(View.VISIBLE);
+                if (this.profile_spin != null) this.profile_spin.setEnabled(!active);
+                if (this.profile_edit != null) this.profile_edit.setVisibility(active ? View.GONE : View.VISIBLE);
             }
             if (prof != null) {
                 if ((UIF_RESET & flags) != 0) {
@@ -1070,35 +1070,35 @@ private void renameOvpnFilesToEncoded() {
                 }
                 EditText focus = null;
                 if (!active && (flags & 32) != 0) {
-                    this.post_import_help_blurb.setVisibility(View.VISIBLE);
+                    if (this.post_import_help_blurb != null) this.post_import_help_blurb.setVisibility(View.VISIBLE);
                 } else if (active) {
-                    this.post_import_help_blurb.setVisibility(View.GONE);
+                    if (this.post_import_help_blurb != null) this.post_import_help_blurb.setVisibility(View.GONE);
                 }
                 ProxyList proxy_list = get_proxy_list();
                 if (active || proxy_list == null || proxy_list.size() <= 0) {
-                    this.proxy_group.setVisibility(View.GONE);
+                    if (this.proxy_group != null) this.proxy_group.setVisibility(View.GONE);
                 } else {
                     SpinUtil.show_spinner(this, this.proxy_spin, proxy_list.get_name_list(true));
                     String name = proxy_list.get_enabled(true);
                     if (name != null) {
                         SpinUtil.set_spinner_selected_item(this.proxy_spin, name);
                     }
-                    this.proxy_group.setVisibility(View.VISIBLE);
+                    if (this.proxy_group != null) this.proxy_group.setVisibility(View.VISIBLE);
                 }
                 if (active || !prof.server_list_defined()) {
-                    this.server_group.setVisibility(View.GONE);
+                    if (this.server_group != null) this.server_group.setVisibility(View.GONE);
                 } else {
                     SpinUtil.show_spinner(this, this.server_spin, prof.get_server_list().display_names());
                     String server = this.prefs.get_string_by_profile(prof.get_name(), "server");
                     if (server != null) {
                         SpinUtil.set_spinner_selected_item(this.server_spin, server);
                     }
-                    this.server_group.setVisibility(View.VISIBLE);
+                    if (this.server_group != null) this.server_group.setVisibility(View.VISIBLE);
                 }
                 if (active) {
-                    this.username_group.setVisibility(View.GONE);
-                    this.pk_password_group.setVisibility(View.GONE);
-                    this.password_group.setVisibility(View.GONE);
+                    if (this.username_group != null) this.username_group.setVisibility(View.GONE);
+                    if (this.pk_password_group != null) this.pk_password_group.setVisibility(View.GONE);
+                    if (this.password_group != null) this.password_group.setVisibility(View.GONE);
                 } else {
                     boolean is_pwd_save;
                     String saved_pwd;
@@ -1108,50 +1108,56 @@ private void renameOvpnFilesToEncoded() {
                     boolean dynamic_challenge = prof.is_dynamic_challenge();
                     if ((!autologin || udef) && !dynamic_challenge) {
                         if (udef) {
-                            this.username_edit.setText(prof.get_userlocked_username());
-                            set_enabled(this.username_edit, RETAIN_AUTH);
+                            if (this.username_edit != null) {
+                                this.username_edit.setText(prof.get_userlocked_username());
+                                set_enabled(this.username_edit, RETAIN_AUTH);
+                            }
                         } else {
-                            set_enabled(this.username_edit, true);
-                            String pref_username = this.prefs.get_string_by_profile(prof.get_name(), "username");
-                            if (pref_username != null) {
-                                this.username_edit.setText(pref_username);
-                            } else {
-                                focus = this.username_edit;
+                            if (this.username_edit != null) {
+                                set_enabled(this.username_edit, true);
+                                String pref_username = this.prefs.get_string_by_profile(prof.get_name(), "username");
+                                if (pref_username != null) {
+                                    this.username_edit.setText(pref_username);
+                                } else {
+                                    focus = this.username_edit;
+                                }
                             }
                         }
-                        this.username_group.setVisibility(View.VISIBLE);
+                        if (this.username_group != null) this.username_group.setVisibility(View.VISIBLE);
                     } else {
-                        this.username_group.setVisibility(View.GONE);
+                        if (this.username_group != null) this.username_group.setVisibility(View.GONE);
                     }
                     if (pk_pwd_req) {
                         is_pwd_save = this.prefs.get_boolean_by_profile(prof.get_name(), "pk_password_save", RETAIN_AUTH);
                         saved_pwd = null;
-                        this.pk_password_group.setVisibility(View.VISIBLE);
-                        this.pk_password_save_checkbox.setChecked(is_pwd_save);
+                        if (this.pk_password_group != null) this.pk_password_group.setVisibility(View.VISIBLE);
+                        if (this.pk_password_save_checkbox != null) this.pk_password_save_checkbox.setChecked(is_pwd_save);
                         if (is_pwd_save) {
                             saved_pwd = this.pwds.get("pk", prof.get_name());
                         }
-                        if (saved_pwd != null) {
+                        if (saved_pwd != null && this.pk_password_edit != null) {
                             this.pk_password_edit.setText(saved_pwd);
                         } else if (focus == null) {
                             focus = this.pk_password_edit;
                         }
                     } else {
-                        this.pk_password_group.setVisibility(View.GONE);
+                        if (this.pk_password_group != null) this.pk_password_group.setVisibility(View.GONE);
                     }
                     if (autologin || dynamic_challenge) {
-                        this.password_group.setVisibility(View.GONE);
+                        if (this.password_group != null) this.password_group.setVisibility(View.GONE);
                     } else {
                         boolean is_auth_pw_save = prof.get_allow_password_save();
                         is_pwd_save = (is_auth_pw_save && this.prefs.get_boolean_by_profile(prof.get_name(), "auth_password_save", RETAIN_AUTH));
                         saved_pwd = null;
-                        this.password_group.setVisibility(View.VISIBLE);
-                        this.password_save_checkbox.setEnabled(is_auth_pw_save);
-                        this.password_save_checkbox.setChecked(is_pwd_save);
+                        if (this.password_group != null) this.password_group.setVisibility(View.VISIBLE);
+                        if (this.password_save_checkbox != null) {
+                            this.password_save_checkbox.setEnabled(is_auth_pw_save);
+                            this.password_save_checkbox.setChecked(is_pwd_save);
+                        }
                         if (is_pwd_save) {
                             saved_pwd = this.pwds.get("auth", prof.get_name());
                         }
-                        if (saved_pwd != null) {
+                        if (saved_pwd != null && this.password_edit != null) {
                             this.password_edit.setText(saved_pwd);
                         } else if (focus == null) {
                             focus = this.password_edit;
@@ -1159,53 +1165,66 @@ private void renameOvpnFilesToEncoded() {
                     }
                 }
                 if (active || prof.get_autologin() || !prof.challenge_defined()) {
-                    this.cr_group.setVisibility(View.GONE);
+                    if (this.cr_group != null) this.cr_group.setVisibility(View.GONE);
                 } else {
-                    this.cr_group.setVisibility(View.VISIBLE);
+                    if (this.cr_group != null) this.cr_group.setVisibility(View.VISIBLE);
                     Challenge chal = prof.get_challenge();
-                    this.challenge_view.setText(chal.get_challenge());
-                    this.challenge_view.setVisibility(View.VISIBLE);
+                    if (this.challenge_view != null) {
+                        this.challenge_view.setText(chal.get_challenge());
+                        this.challenge_view.setVisibility(View.VISIBLE);
+                    }
                     if (chal.get_response_required()) {
-                        if (chal.get_echo()) {
-                            this.response_edit.setTransformationMethod(SingleLineTransformationMethod.getInstance());
-                        } else {
-                            this.response_edit.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                        if (this.response_edit != null) {
+                            if (chal.get_echo()) {
+                                this.response_edit.setTransformationMethod(SingleLineTransformationMethod.getInstance());
+                            } else {
+                                this.response_edit.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                            }
+                            this.response_edit.setVisibility(View.VISIBLE);
                         }
-                        this.response_edit.setVisibility(View.VISIBLE);
                         if (focus == null) {
                             focus = this.response_edit;
                         }
                     } else {
-                        this.response_edit.setVisibility(View.GONE);
+                        if (this.response_edit != null) this.response_edit.setVisibility(View.GONE);
                     }
                     if (prof.is_dynamic_challenge()) {
                         schedule_ui_reset(prof.get_dynamic_challenge_expire_delay());
                     }
                 }
-                this.button_group.setVisibility(View.VISIBLE);
+                
+                // --- ส่วนจัดการปุ่มกดและสถานะ ---
+                if (this.button_group != null) this.button_group.setVisibility(View.VISIBLE);
+                if (this.status_view != null) this.status_view.setVisibility(View.VISIBLE);
+                
                 if (orig_active) {
-                    this.conn_details_group.setVisibility(View.VISIBLE);
-                    this.connect_button.setVisibility(View.GONE);
-                    this.disconnect_button.setVisibility(View.VISIBLE);
+                    if (this.conn_details_group != null) this.conn_details_group.setVisibility(View.VISIBLE);
+                    if (this.connect_button != null) this.connect_button.setVisibility(View.GONE);
+                    if (this.disconnect_button != null) this.disconnect_button.setVisibility(View.VISIBLE);
                 } else {
-                    this.conn_details_group.setVisibility(View.GONE);
-                    this.connect_button.setVisibility(View.VISIBLE);
-                    this.disconnect_button.setVisibility(View.GONE);
+                    if (this.conn_details_group != null) this.conn_details_group.setVisibility(View.GONE);
+                    if (this.connect_button != null) this.connect_button.setVisibility(View.VISIBLE);
+                    if (this.disconnect_button != null) this.disconnect_button.setVisibility(View.GONE);
                 }
                 if (focus != null) {
                     autostart = RETAIN_AUTH;
                 }
                 req_focus(focus);
             } else {
-                this.post_import_help_blurb.setVisibility(View.GONE);
-                this.proxy_group.setVisibility(View.GONE);
-                this.server_group.setVisibility(View.GONE);
-                this.username_group.setVisibility(View.GONE);
-                this.pk_password_group.setVisibility(View.GONE);
-                this.password_group.setVisibility(View.GONE);
-                this.cr_group.setVisibility(View.GONE);
-                this.conn_details_group.setVisibility(View.GONE);
-                this.button_group.setVisibility(View.GONE);
+                if (this.post_import_help_blurb != null) this.post_import_help_blurb.setVisibility(View.GONE);
+                if (this.proxy_group != null) this.proxy_group.setVisibility(View.GONE);
+                if (this.server_group != null) this.server_group.setVisibility(View.GONE);
+                if (this.username_group != null) this.username_group.setVisibility(View.GONE);
+                if (this.pk_password_group != null) this.pk_password_group.setVisibility(View.GONE);
+                if (this.password_group != null) this.password_group.setVisibility(View.GONE);
+                if (this.cr_group != null) this.cr_group.setVisibility(View.GONE);
+                if (this.conn_details_group != null) this.conn_details_group.setVisibility(View.GONE);
+                
+                // เปิดให้แสดงปุ่มกดเพื่อกดเชื่อมต่อได้ แม้ระบบยังอ่านข้อมูลโปรไฟล์ย่อยไม่สมบูรณ์
+                if (this.button_group != null) this.button_group.setVisibility(View.VISIBLE);
+                if (this.connect_button != null) this.connect_button.setVisibility(View.VISIBLE);
+                if (this.disconnect_button != null) this.disconnect_button.setVisibility(View.GONE);
+                
                 show_status_icon(R.drawable.info);
                 show_status(R.string.no_profiles_loaded);
             }
@@ -1221,7 +1240,7 @@ private void renameOvpnFilesToEncoded() {
             start_connect();
         }
     }
-
+    
     private void set_enabled(EditText editText, boolean state) {
         editText.setEnabled(state);
         editText.setFocusable(state);
